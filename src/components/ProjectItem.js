91c9d3431907f5274694c1f28c285d2f693a9c3a
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-05-06 16:09:18 -0700

    add github button animations

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,7 +4,15 @@ import { Fade } from "react-reveal";
 import Ionicon from "react-ionicons";
 
 class ProjectItem extends React.Component {
-  state = {};
+  state = {
+    isHovered: false
+  };
+
+  handleHover() {
+    this.setState({
+      isHovered: !this.state.isHovered
+    });
+  }
 
   componentDidMount() {}
 
@@ -13,6 +21,8 @@ class ProjectItem extends React.Component {
   componentWillUnmount() {}
 
   render() {
+    const btnClass = this.state.isHovered ? "pulse animated project-github-icon-hover" : "project-github-icon";
+
     return (
       <Fade>
         <div className="project">
@@ -24,21 +34,20 @@ class ProjectItem extends React.Component {
             />
             <h2 className="project_title"> {this.props.name} </h2>
           </a>
-          <h3 className="project_description">
-            {this.props.description}
-          </h3>
-          <p className="project_tech">
-            {this.props.builtWith}
-          </p>
-          <a href={this.props.github}>
-          <Ionicon
-            icon="logo-github"
-            color="#EEEEEE"
-            fontSize="4em"
-            beat={false}
-            // onClick={() => this.handleRoute("/")}
-          />
-          </a>
+          <h3 className="project_description">{this.props.description}</h3>
+          <p className="project_tech">{this.props.builtWith}</p>
+          <div onMouseEnter={this.handleHover.bind(this)} onMouseLeave={this.handleHover.bind(this)}>
+            <a href={this.props.github}>
+              <Ionicon
+                icon="logo-github"
+                color={this.state.isHovered ? "#00adb5" : "#EEEEEE"}
+                fontSize="4em"
+                beat={false}
+                // onClick={() => this.handleRoute("/")}
+                className={btnClass}
+              />
+            </a>
+          </div>
         </div>
       </Fade>
     );
