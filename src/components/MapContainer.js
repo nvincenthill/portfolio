ea849df8b070e4948971b6376a7f88884c6eb5a9
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-05-06 22:07:46 -0700

    add header component

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,25 +1,34 @@
 import { GoogleApiWrapper } from "google-maps-react";
 import GoogleMap from "./GoogleMap";
 import React from "react";
-
+import { Fade } from "react-reveal";
 export class MapContainer extends React.Component {
   render() {
     const style = {
-          width: '100vw',
-          height: '50vh',
-          position: "inline",
-          align: "center"
-        }
+      width: "100vw",
+      height: "50vh",
+      position: "inline",
+      align: "center"
+    };
     return (
       <div className="map-container" style={style}>
-        <div className="contact-greeting-container"> 
-          <h2 className="contact-greeting-heading">Greetings! </h2> 
-          <p className="contact-greeting-body"> I'd love to talk to you! <br /> Please send me an email <br /> and I'll get back to you <br /> as soon as possible.</p>
-          <h2 className="contact-greeting-footer"> -Nick </h2> 
+        <div className="contact-greeting-container">
+          <h2 className="contact-greeting-heading">Greetings! </h2>
+          <p className="contact-greeting-body">
+            {" "}
+            I'd love to talk to you! <br /> Please send me an email <br /> and
+            I'll get back to you <br /> as soon as possible.
+          </p>
+          <h2 className="contact-greeting-footer"> -Nick </h2>
         </div>
-        <GoogleMap key={this.props.mapType} style={this.props.style} mapType={this.props.mapType}  google={this.props.google} />
+          <GoogleMap
+            key={this.props.mapType}
+            style={this.props.style}
+            mapType={this.props.mapType}
+            google={this.props.google}
+          />
       </div>
-    )
+    );
   }
 }
 
