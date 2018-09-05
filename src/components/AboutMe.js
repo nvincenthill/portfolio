bc434b175cbcb02589fa975f35709add0310fc63
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-09-05 10:12:30 -0700

    Update AboutMe page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -30,19 +30,35 @@ class AboutMe extends React.Component {
               <Fade cascade>
                 <div>
                   <p className="about-description">
-                    I started my career at a hedge fund as a trader - I've
-                    traded equities, options, commodities, credit,
-                    morgage-backed securities, and other derivatives.
+                    I'm a full-stack software engineer who enjoys building
+                    scalable, responsive, mobile-first web applications.
                   </p>
+                  <p className="about-description">
+                    In a prior life as a trader, I hedged interest rate risk for
+                    mortgage originators with mortgage-backed securities. I've
+                    also traded commodities, equities, and derivatives. As a
+                    developer, I use my capital markets experience to identify
+                    and solve business problems with beautiful web applications.
+                  </p>
+                  <p className="about-description">
+                    When I’m not on the job, I enjoy traveling the world and
+                    urban gardening with my girlfriend Melody.
+                  </p>
+
+                  <p className="about-description">
+                    I have experience working with: React, MongoDB, Express,
+                    Node.js, Git, Docker, Webpack, SQL, HTML5/CSS/JS, AWS (S3,
+                    EC2, Route 53, CloudFront), Jest/Enzyme, Mocha/Chai, Redux,
+                    jQuery, Heroku, CircleCI, p5.js, SQLite, MySQL, PostgresSQL,
+                    Grunt, and Linux.
+                  </p>
+
                   <p className="about-description">
                     <span className="about-hook">
-                      {" "}
-                      ... but it wasn't my passion.{" "}
+                      I'd love to work with you and add value to your firm.
                     </span>
                   </p>
-                  <p className="about-description">
-                    I'd love to work with you and add value to your firm.
-                  </p>
+
                   <div className="about-contact">
                     <Link to="/Contact/">
                       <Ionicon
